5eae15af81a30145dbf9db41d945448876c22e9a
dQw4w <b11901095@ntu.edu.tw>
2026-06-23 20:40:12 +0800

    oihwefoiw

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import TeamPage from './pages/TeamPage'
 import AdminPage from './pages/AdminPage'
 import QrPrintPage from './pages/QrPrintPage'
@@ -12,8 +12,6 @@ export default function App() {
           <Route path="/team/:token" element={<TeamPage />} />
           <Route path="/admin" element={<AdminPage />} />
           <Route path="/admin/qr" element={<QrPrintPage />} />
-          <Route path="/" element={<Navigate to="/admin" replace />} />
-          <Route path="*" element={<Navigate to="/admin" replace />} />
         </Routes>
       </BrowserRouter>
     </ErrorBoundary>
